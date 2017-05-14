fa0fb82c678ba216ecd61b8bd2073375e69ed8a9
Marcelo Riephoff <mriephoff@hotmail.com>
2017-05-14 15:21:52 -0300

    DAL ciudades  los archivos de BL

--- Data Access Layer/DALCiudadesController.cs
+++ Data Access Layer/DALCiudadesController.cs
@@ -1,10 +1,60 @@
-﻿using System;
+﻿using Shared.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Data_Access_Layer {
-    class DALCiudadesController {
+    public class DALCiudadesController : IDALCiudades{
+
+        public void AgregarCiudad(Ciudades aux)
+        {
+            using (var db = new Modelo.autenticacionEntities())
+            {
+                db.Ciudades.Add(aux);
+                db.SaveChanges();
+            }
+
+        }
+
+        public void BorrarCiudad(int id)
+        {
+            using (var db = new Modelo.autenticacionEntities())
+            {
+                db.Ciudades.Remove(db.Ciudades.Find(id));
+                db.SaveChanges();
+            }
+        }
+
+        public void ActualizarCiudad(Ciudades aux)
+        {
+            using (var db = new Modelo.autenticacionEntities())
+            {
+                var ciudad = db.Ciudades.Find(aux.CiudadID);
+                ciudad.Nombre = aux.Nombre;
+                ciudad.Radio = aux.Radio;
+                ciudad.CoordenadaX = aux.CoordenadaX;
+                ciudad.URLLogo = aux.URLLogo;
+                db.SaveChanges();
+            }
+        }
+
+        public Ciudades GetCiudad(int id)
+        {
+            using (var db = new Modelo.autenticacionEntities())
+            {
+                return db.Ciudades.Find(id);
+            }
+        }
+
+        public List<Ciudades> ListaCiudades()
+        {
+            using (var db = new Modelo.autenticacionEntities())
+            {
+                return db.Ciudades.ToList();
+            }
+        }
+
     }
 }
--- Data Access Layer/IDALCiudades.cs
+++ Data Access Layer/IDALCiudades.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using Shared.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Data_Access_Layer {
-    interface IDALCiudades {
+    public interface IDALCiudades {
+        void AgregarCiudad(Ciudades aux);
+
+        void BorrarCiudad(int id);
+
+        void ActualizarCiudad(Ciudades aux);
+
+        Ciudades GetCiudad(int id);
+
+        List<Ciudades> ListaCiudades();
     }
 }
