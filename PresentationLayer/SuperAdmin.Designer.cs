bc2f87de80a43f8552a3079568991d8cd1129fa5
Marcelo Riephoff <mriephoff@hotmail.com>
2017-05-13 21:54:57 -0300

    presentacion

--- PresentationLayer/SuperAdmin.Designer.cs
+++ PresentationLayer/SuperAdmin.Designer.cs
@@ -28,20 +28,186 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.agregar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.nombre = new System.Windows.Forms.TextBox();
+            this.correo = new System.Windows.Forms.TextBox();
+            this.contrasena = new System.Windows.Forms.TextBox();
+            this.ciudad = new System.Windows.Forms.TextBox();
+            this.cordY = new System.Windows.Forms.TextBox();
+            this.cordX = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // agregar
+            // 
+            this.agregar.Location = new System.Drawing.Point(122, 143);
+            this.agregar.Name = "agregar";
+            this.agregar.Size = new System.Drawing.Size(75, 23);
+            this.agregar.TabIndex = 0;
+            this.agregar.Text = "agregar";
+            this.agregar.UseVisualStyleBackColor = true;
+            this.agregar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(12, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Contraseña";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(12, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Correo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.Location = new System.Drawing.Point(12, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Ciudad";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label5.Location = new System.Drawing.Point(12, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Coordenadas";
+            // 
+            // nombre
+            // 
+            this.nombre.Location = new System.Drawing.Point(117, 12);
+            this.nombre.Name = "nombre";
+            this.nombre.Size = new System.Drawing.Size(187, 20);
+            this.nombre.TabIndex = 6;
+            // 
+            // correo
+            // 
+            this.correo.Location = new System.Drawing.Point(117, 38);
+            this.correo.Name = "correo";
+            this.correo.Size = new System.Drawing.Size(187, 20);
+            this.correo.TabIndex = 7;
+            // 
+            // contrasena
+            // 
+            this.contrasena.Location = new System.Drawing.Point(117, 64);
+            this.contrasena.Name = "contrasena";
+            this.contrasena.Size = new System.Drawing.Size(187, 20);
+            this.contrasena.TabIndex = 8;
+            // 
+            // ciudad
+            // 
+            this.ciudad.Location = new System.Drawing.Point(117, 90);
+            this.ciudad.Name = "ciudad";
+            this.ciudad.Size = new System.Drawing.Size(187, 20);
+            this.ciudad.TabIndex = 9;
+            // 
+            // cordY
+            // 
+            this.cordY.Location = new System.Drawing.Point(243, 116);
+            this.cordY.Name = "cordY";
+            this.cordY.Size = new System.Drawing.Size(61, 20);
+            this.cordY.TabIndex = 10;
+            // 
+            // cordX
+            // 
+            this.cordX.Location = new System.Drawing.Point(150, 116);
+            this.cordX.Name = "cordX";
+            this.cordX.Size = new System.Drawing.Size(61, 20);
+            this.cordX.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label6.Location = new System.Drawing.Point(127, 117);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "X";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label7.Location = new System.Drawing.Point(220, 117);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Y";
             // 
             // SuperAdmin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(317, 181);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cordX);
+            this.Controls.Add(this.cordY);
+            this.Controls.Add(this.ciudad);
+            this.Controls.Add(this.contrasena);
+            this.Controls.Add(this.correo);
+            this.Controls.Add(this.nombre);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.agregar);
             this.Name = "SuperAdmin";
             this.Text = "SuperAdmin";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button agregar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox nombre;
+        private System.Windows.Forms.TextBox correo;
+        private System.Windows.Forms.TextBox contrasena;
+        private System.Windows.Forms.TextBox ciudad;
+        private System.Windows.Forms.TextBox cordY;
+        private System.Windows.Forms.TextBox cordX;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- PresentationLayer/SuperAdmin.cs
+++ PresentationLayer/SuperAdmin.cs
@@ -7,14 +7,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
 
 namespace PresentationLayer
 {
     public partial class SuperAdmin : Form
     {
+        private static string auxEx;
+
         public SuperAdmin()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EnviarMensaje(correo.Text);
+        }
+
+        public static bool EnviarMensaje(string dir)
+        {
+            try
+            {
+                //Configuración del Mensaje para Gmail
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                //Direccion desde el que se envia el Email
+                mail.From = new MailAddress("soporte.cerebro@gmail.com", "Cerebro", Encoding.UTF8);
+
+                //Asunto del Correo
+                mail.Subject = "Prueba de Envío de Correo";
+
+                //Mensaje del correo
+                mail.Body = "Prueba de Envío de Correo de Gmail desde C# para el barba";
+
+                //Destinatario del Email
+                mail.To.Add(dir);
+                
+                //Configuracion del SMTP
+                SmtpServer.Port = 587; //Gmail Port
+
+                //Credenciales del mail desde el que se envia
+                SmtpServer.Credentials = new System.Net.NetworkCredential("soporte.cerebro@gmail.com", "fantauvacerebro");
+                SmtpServer.EnableSsl = true;
+                SmtpServer.Send(mail);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                auxEx = ex.ToString();
+                return false;
+            }
         }
     }
 }
