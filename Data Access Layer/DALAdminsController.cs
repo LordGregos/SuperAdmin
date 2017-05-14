a53f97d276406ac819c92f5aded9e3daae1f193b
José Calvelo <j.calvelo1993@gmail.com>
2017-05-14 14:07:04 -0300

    Interfaz y Controller de Admins

--- Data Access Layer/DALAdminsController.cs
+++ Data Access Layer/DALAdminsController.cs
@@ -1,10 +1,64 @@
-﻿using System;
+﻿using Shared.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Data_Access_Layer {
-    class DALAdminsController {
+    public class DALAdminsController : IDALAdmins{
+
+        public void AgregarAdmin(Admins adm) {
+            using (var db = new Modelo.autenticacionEntities()) 
+            {
+            db.Admins.Add(adm);
+            db.SaveChanges();
+            }
+
+        }
+
+        public void BorrarAdmin(int id) {
+            using (var db = new Modelo.autenticacionEntities()) 
+            {
+            db.Admins.Remove(db.Admins.Find(id));
+            db.SaveChanges();
+            }
+        }
+
+        public void ActualizarAdmin(Admins adm) {
+            using (var db = new Modelo.autenticacionEntities()) 
+            {
+            var admobj = db.Admins.Find(adm.ID);
+            admobj.Contrasena = adm.Contrasena;
+            admobj.Nombre = adm.Nombre;
+            admobj.utilizado = adm.utilizado;
+            admobj.Ciudad = adm.Ciudad;
+            admobj.Ciudades = adm.Ciudades;
+            db.SaveChanges();
+            }
+        }
+
+        public Admins GetAdmin(int id) {
+            using (var db = new Modelo.autenticacionEntities()) 
+            {
+            return db.Admins.Find(id);
+            }
+        }
+
+        public void AsignarCiudad(int adminID, Ciudades c) {
+            using (var db = new Modelo.autenticacionEntities()) 
+            {
+            var adm = db.Admins.Find(adminID);
+            adm.Ciudad = c.CiudadID;
+            db.SaveChanges();
+            }
+        }
+
+        public List<Admins> ListaAdmins() {
+            using (var db = new Modelo.autenticacionEntities()) 
+            {
+            return db.Admins.ToList();
+            }
+        }
     }
 }
--- Data Access Layer/IDALAdmins.cs
+++ Data Access Layer/IDALAdmins.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using Shared.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Data_Access_Layer {
-    interface IDALAdmins {
+    public interface IDALAdmins {
+        void AgregarAdmin(Admins adm);
+
+        void BorrarAdmin(int id);
+
+        void ActualizarAdmin(Admins adm);
+
+        Admins GetAdmin(int id);
+
+        void AsignarCiudad(int adminID, Ciudades c);
+
+        List<Admins> ListaAdmins();
     }
 }
