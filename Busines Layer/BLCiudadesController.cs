540609f83c306da6c46e620b727079b7ccd3ce35
Marcelo Riephoff <mriephoff@hotmail.com>
2017-05-14 15:25:20 -0300

    coso pum

--- Busines Layer/BLCiudadesController.cs
+++ Busines Layer/BLCiudadesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Shared.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Busines_Layer
 {
-    class BLCiudadesController
+    public class BLCiudadesController : IBLCiudades
     {
     }
 }
--- Busines Layer/IBLCiudades.cs
+++ Busines Layer/IBLCiudades.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Shared.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace Busines_Layer
 {
-    class IBLCiudades
+    public interface IBLCiudades
     {
+        void AgregarCiudad(Ciudades aux);
+
+        void BorrarCiudad(int id);
+
+        void ActualizarCiudad(Ciudades aux);
+
+        Ciudades GetCiudad(int id);
+
+        List<Ciudades> ListaCiudades();
     }
 }
