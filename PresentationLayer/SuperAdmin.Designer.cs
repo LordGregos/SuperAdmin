9baffed788a9b2eef094f97580b888bb52cdf9c5
Marcelo Riephoff <mriephoff@hotmail.com>
2017-06-12 03:33:02 -0300

    barba.. no deja guardar..
    no se porque explota..

--- PresentationLayer/SuperAdmin.Designer.cs
+++ PresentationLayer/SuperAdmin.Designer.cs
@@ -30,7 +30,6 @@
         {
             this.agregar = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.nombre = new System.Windows.Forms.TextBox();
             this.correo = new System.Windows.Forms.TextBox();
@@ -56,16 +55,6 @@
             this.label1.Size = new System.Drawing.Size(58, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Nombre";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label2.Location = new System.Drawing.Point(12, 64);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(81, 17);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Contraseña";
             // 
             // label3
             // 
@@ -107,7 +96,6 @@
             this.Controls.Add(this.correo);
             this.Controls.Add(this.nombre);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.agregar);
             this.Name = "SuperAdmin";
@@ -121,7 +109,6 @@
 
         private System.Windows.Forms.Button agregar;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox nombre;
         private System.Windows.Forms.TextBox correo;
